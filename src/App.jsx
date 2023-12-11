6545e635f120838595ac71e523e541e498949fa7
Ignacio Aguirre <aguirreignacio.012@gmail.com>
2023-12-11 15:06:07 -0300

    Añadido estilos al componente Button

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Form from './components/Form/Form'
 function App() {
   return (
     <>
-      <div>
+      <div className='infoSection'>
         <h1>Learn to code by watching others </h1>
         <p>
           See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
@@ -13,7 +13,7 @@ function App() {
         </p>
       </div>
       <div>
-        <Button txt='Try it free 7 days then $20/mo. thereafter' />
+        <Button home boldText='Try it free 7 days' normalText=' then $20/mo. thereafter' />
       </div>
 
       <div>

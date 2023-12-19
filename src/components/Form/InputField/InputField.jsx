483a29173a3ed9421838e8b804ad5cf4679e957d
Ignacio Aguirre <aguirreignacio.012@gmail.com>
2023-12-19 20:01:39 -0300

    Agregar estilos al componente InputField

--- src/components/Form/InputField/InputField.jsx
+++ src/components/Form/InputField/InputField.jsx
@@ -1,10 +1,28 @@
-const InputField = ({ label, type, name, value, onChange }) => {
-  return (
-    <label htmlFor={name}>
-      {label}
-      <input type={type} name={name} placeholder={label} value={value} onChange={onChange} />
-    </label>
+import styled from "styled-components";
 
+const StyledInput = styled.input`
+width:100%;
+font-size:1rem;
+outline:none;
+font-weight:600;
+padding:1em 1.2em;
+position:relative;
+background-color: #fff;
+border-radius:.60em;
+border: 2px solid  rgba(0,0,0,15%);
+color: hsl(249, 10%, 26%) ;
+
+
+&:focus{
+border: 2px solid  hsl(246, 25%, 77%) ;
+}
+`
+
+const InputField = ({ type, name, placeholder, value, onChange }) => {
+  return (
+    <div>
+      <StyledInput type={type} name={name} placeholder={placeholder} value={value} onChange={onChange} />
+    </div>
   );
 }
 

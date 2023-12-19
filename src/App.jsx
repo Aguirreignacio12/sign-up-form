52a73fdf1ef54b91e868d12017b9a11ffc06ea35
Ignacio Aguirre <aguirreignacio.012@gmail.com>
2023-12-19 20:36:46 -0300

    Implementar diseño mobile-first

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import './App.css'
+import styled from 'styled-components'
 import Button from './components/Button/Button'
 import Form from './components/Form/Form'
+import Info from './components/Info/Info'
+import Footer from './components/Footer/Footer'
 
-function App() {
+const StyledApp = styled.div`
+  width:100%;
+  max-width: 1280px;
+  min-height:100vh;
+  margin: 0 auto;
+  padding: 2rem;
+  gap:1.5em;
+  text-align: center;
+  display:grid;
+  grid-template-columns:repeat(4,1fr);
+  grid-template-rows: auto;
+  grid-template-areas:
+    "info info info info"
+    "buttonInfo buttonInfo buttonInfo buttonInfo"
+    "form form form form"
+    "footer footer footer footer";
+`
+
+const StyledContainer = styled.div`
+
+
+grid-area: ${({ $gridArea }) => $gridArea || 'auto'}`
+const App = () => {
   return (
-    <>
-      <div className='infoSection'>
-        <h1>Learn to code by watching others </h1>
-        <p>
-          See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
-          but understanding how developers think is invaluable.
-        </p>
-      </div>
-      <div>
-        <Button home boldText='Try it free 7 days' normalText=' then $20/mo. thereafter' />
-      </div>
+    <StyledApp>
+      <StyledContainer $gridArea='info'>
+        <Info />
+      </StyledContainer>
 
-      <div>
+      <StyledContainer $gridArea='buttonInfo'>
+        <Button boldText='Try it free 7 days' normalText=' then $20/mo. thereafter' />
+      </StyledContainer>
+
+      <StyledContainer $gridArea='form'>
         <Form />
+      </StyledContainer>
 
-      </div>
-
-      <footer>
-        <p className="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-          Coded by <a href="#">Your Name Here</a>.
-        </p>
-      </footer>
-
-    </>
+      <StyledContainer $gridArea='footer'>
+        <Footer />
+      </StyledContainer>
+    </StyledApp>
   )
 }
 
